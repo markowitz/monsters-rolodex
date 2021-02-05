158386740b765fd1994d65aa96be648f00daa0e2
Ikechukwu Nwakanma <icnwakanma@gmail.com>
2021-02-05 15:21:43 +0100

    monster rolodex

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,12 @@
+import './card.styles.css';
+
+export const Card = props =>  (
+     <div className="card-container">
+         <img src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}  alt="" />
+         <h3>
+         {props.monster.name}
+         </h3>
+         <p>{props.monster.email}</p>
+    </div>
+
+)
